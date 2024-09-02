3b1b36bb726f62984e7abce68ca61fe8ef029b9f
Simone Scaccia <simonescaccia.s@gmail.com>
2024-09-02 17:44:58 +0200

    Add unpie train methods

--- main.py
+++ main.py
@@ -5,16 +5,29 @@ import os
 from dataset.pie_data import PIE
 from model.unpie import UnPIE
 
-if __name__ == '__main__':
-
-    # Setup environment
+def get_config_file():
     with open('config.yml', 'r') as file:
         config_file = yaml.safe_load(file)
+    return config_file
 
+def set_environment(config_file):
     if not config_file['IS_GPU']:
         os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
+def get_params(config_file):
+    params = {
+        'pie_path': config_file['PIE_PATH']
+    }
+    return params
+
+if __name__ == '__main__':
+
+    # Setup environment
+    config_file = get_config_file()
+    set_environment(config_file)
+
     # Laod parameters
+    params = get_params()
 
     # Train and/or test
     train_test = int(sys.argv[1]) # 0: train and test, 1: test only
