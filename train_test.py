585563ba6ee2138d48e3d569fcbeccd0d5d4f8e6
Simone Scaccia <simonescaccia.s@gmail.com>
2024-10-06 15:02:49 +0200

    Update unpie

--- train_test.py
+++ train_test.py
@@ -5,20 +5,23 @@ from model.params_loader import ParamsLoader
 from model.unpie import UnPIE
 from utils.print_utils import print_separator
 
+def get_unpie(phase, training_step):
+    params_loader = ParamsLoader(training_step)
+    pie_preprocessing = PIEPreprocessing(params_loader.get_pie_params())
+    data_loaders = pie_preprocessing.get_data_loaders(phase)
+    params = params_loader.get_params(data_loaders, phase)
+    unpie = UnPIE(params)
+    return unpie
 
 if __name__ == '__main__':
     train_test = int(sys.argv[1]) # 0: train only, 1: train and test, 2: test only
     training_step = sys.argv[2]
     print_separator('UnPIE started, step: ' + training_step, top_new_line=False)
 
-    params_loader = ParamsLoader(training_step)
-    pie_preprocessing = PIEPreprocessing(params_loader.get_pie_params())
-    train_data_loader, val_data_loader = pie_preprocessing.get_dataloaders()
-    params = params_loader.get_params(train_data_loader, val_data_loader)
-    unpie = UnPIE(params)
-
     if train_test < 2:
+        unpie = get_unpie('train', training_step)
         unpie.train()
     if train_test > 0:
+        unpie = get_unpie('test', training_step)
         unpie.test()
         print_separator('UnPIE ended', bottom_new_line=False)
