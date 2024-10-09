64468d61882e488873b4cf007dbc95111c4b0632
Simone Scaccia <simonescaccia.s@gmail.com>
2024-10-09 03:10:54 +0200

    Update unpie

--- plot_creator.py
+++ plot_creator.py
@@ -4,6 +4,7 @@ import sys
 import re
 import pandas as pd
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 from model.params_loader import ParamsLoader
 from utils.print_utils import print_separator
 
@@ -12,30 +13,34 @@ df_tot_val = pd.DataFrame(columns=['step', 'top1'])
 
 val_step = 0
 training_steps = sys.argv[1:]
-for training_step in training_steps:
+for i in tqdm(range(len(training_steps))):
     df_val = pd.DataFrame(columns=['step', 'top1'])
     df_train = pd.DataFrame(columns=['step', 'loss'])
-    params_loader = ParamsLoader(training_step)
+    params_loader = ParamsLoader(training_steps[i])
     params = params_loader.get_plot_params()
     cache_dir = params['cache_dir'] # Set cache directory
     log_file_path = os.path.join(cache_dir, params['train_log_file'])
     val_log_file_path = os.path.join(cache_dir, params['val_log_file'])
-    with open(log_file_path, 'r') as file:
-        for line in file:
-            match = re.search(r'Step (\d+).*loss: ([\d\.]+)', line)
-            step = int(match.group(1))
-            loss = float(match.group(2))
-            df_train = df_train.append({'step': step, 'loss': loss}, ignore_index=True)
-    with open(val_log_file_path, 'r') as file:
-        for line in file:
-            line_strip = line.strip()
-            line_split = line_strip.split('topn: ')[1]
-            topn_dict = ast.literal_eval(line_split)
-            key = list(topn_dict.keys())[0]
-            frequency = int(key.split('_')[1].split('NN')[0])
-            top1 = float(list(topn_dict.values())[0])
-            df_val = df_val.append({'step': val_step, 'top1': top1}, ignore_index=True)
-            val_step += frequency
+    with tqdm(total=os.path.getsize(log_file_path)) as pbar:
+        with open(log_file_path, 'r') as file:
+            for line in file:
+                pbar.update(len(line))
+                match = re.search(r'Step (\d+).*loss: ([\d\.]+)', line)
+                step = int(match.group(1))
+                loss = float(match.group(2))
+                df_train = df_train.append({'step': step, 'loss': loss}, ignore_index=True)
+    with tqdm(total=os.path.getsize(val_log_file_path)) as pbar:
+        with open(val_log_file_path, 'r') as file:
+            for line in file:
+                pbar.update(len(line))
+                line_strip = line.strip()
+                line_split = line_strip.split('topn: ')[1]
+                topn_dict = ast.literal_eval(line_split)
+                key = list(topn_dict.keys())[0]
+                frequency = int(key.split('_')[1].split('NN')[0])
+                top1 = float(list(topn_dict.values())[0])
+                df_val = df_val.append({'step': val_step, 'top1': top1}, ignore_index=True)
+                val_step += frequency
     df_tot_val = df_tot_val.append(df_val, ignore_index=True)
     
     # save the plot of df_train
@@ -55,7 +60,7 @@ for training_step in training_steps:
     plt.savefig(os.path.join(cache_dir, 'val_metric.png'))
 
 # save the plot of df_val
-tot_cache_dir = os.path.join(os.path.split(cache_dir)[0], 'tot_metrics')
+tot_cache_dir = os.path.join(os.path.split(os.path.split(cache_dir)[0])[0], 'tot_metrics', os.path.split(cache_dir)[1])
 os.makedirs(tot_cache_dir, exist_ok=True)
 plt.figure()
 plt.plot(df_tot_val['step'], df_tot_val['top1'])
