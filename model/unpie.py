733b0eb7eabd6ffe49fab5034ae3d4484a46b20f
Simone Scaccia <simonescaccia.s@gmail.com>
2024-11-02 17:36:46 +0100

    Migrate code to tf2

--- model/unpie.py
+++ model/unpie.py
@@ -5,6 +5,7 @@ import numpy as np
 import tensorflow as tf
 
 from utils.print_utils import print_separator
+from utils.vie_utils import tuple_get_one
 
 class UnPIE(tf.Module):
     def __init__(self, params, **kwargs):
@@ -38,39 +39,6 @@ class UnPIE(tf.Module):
                 train=train,
                 **model_func_params)
         return outputs
-
-    def build_train_opt(self, outputs):
-        loss_params = self.params['loss_params']
-
-        loss_retval = self._loss_func(
-            outputs,
-            **loss_params.get('loss_func_kwargs', {}))
-        loss_retval = self._reg_loss(
-            loss_retval,
-            **loss_params.get('agg_func_kwargs', {}))
-
-        lr_rate_params = self.params['learning_rate_params']
-        learning_rate = self._get_lr_from_boundary_and_ramp_up(
-            **lr_rate_params)
-
-        opt_params = self.params['optimizer_params']
-        train_opt = tf.keras.optimizers.SDG(
-            learning_rate=learning_rate, 
-            **opt_params)
-        
-        return loss_retval, learning_rate, train_opt 
-            
-    def build_train_targets(self, outputs, loss_retval, learning_rate, train_opt):
-        extra_targets_params = self.params['train_params']['targets']
-        train_targets = self._rep_loss_func(
-            outputs, 
-            **extra_targets_params)
-
-        train_targets['train_op'] = train_opt
-        train_targets['loss'] = loss_retval
-        train_targets['learning_rate'] = learning_rate
-
-        return train_targets
 
     def load_from_ckpt(self, ckpt_path):
         print('\nRestore from %s' % ckpt_path)
@@ -124,21 +92,6 @@ class UnPIE(tf.Module):
                                     + str(saved_shape))
                 _load_saver = tf.compat.v1.train.Saver(var_list=filtered_var_list)
                 _load_saver.restore(self.sess, ckpt_path)
-
-    def run_each_validation(self, val_key):
-        agg_res = None
-        num_steps = self.params['validation_params'][val_key]['num_steps']
-        for _step in tqdm.trange(num_steps, desc=val_key):
-            if self.params['validation_params'][val_key].get('valid_loop', None) is None:
-                res = self.sess.run(self.all_val_targets[val_key])
-            else:
-                res = self.params['validation_params'][val_key]['valid_loop']['func'](
-                        self.sess, self.all_val_targets[val_key])
-            online_func = self.params['validation_params'][val_key]['online_agg_func']
-            agg_res = online_func(agg_res, res, _step)
-        agg_func = self.params['validation_params'][val_key]['agg_func']
-        val_result = agg_func(agg_res)
-        return val_result
     
     def run_testing(self, test_key):
         agg_res = None
@@ -155,14 +108,75 @@ class UnPIE(tf.Module):
         test_result = agg_func(agg_res)
         return test_result
 
-    def train_func(self, inputs, train):
+    def build_test_inputs(self, test_key):
+        data_params = self.params['test_params'][test_key]['data_params']
+        func = data_params.pop('func')
+        test_inputs = func(**data_params)
+        return test_inputs
+    
+    def build_test_network(self, test_key, test_inputs):
+        with tf.name_scope('test/' + test_key):
+            test_outputs = self.build_network(test_inputs, False)
+        return test_outputs
+
+    def build_test(self):
+        tf.compat.v1.get_variable_scope().reuse_variables()
+        self.all_test_targets = {}
+        for each_val_key in self.params['test_params']:
+            test_inputs = self.build_test_inputs(each_val_key)
+            test_outputs = self.build_test_network(each_val_key, test_inputs)
+            test_targets = self.build_test_targets(
+                    each_val_key, test_inputs, test_outputs)
+            self.all_test_targets[each_val_key] = test_targets           
+
+    def build_model(self):
+        if self.params['is_test']:
+            self.build_test()
+
+        self.build_sess_and_saver()
+        self.init_and_restore()
+
+    # Training functions
+    def build_train_opt(self, outputs):
+        loss_params = self.params['loss_params']
+
+        loss_retval = self._loss_func(
+            outputs,
+            **loss_params.get('loss_func_kwargs', {}))
+        loss_retval = self._reg_loss(
+            loss_retval,
+            **loss_params.get('agg_func_kwargs', {}))
+
+        lr_rate_params = self.params['learning_rate_params']
+        learning_rate = self._get_lr_from_boundary_and_ramp_up(
+            **lr_rate_params)
+
+        opt_params = self.params['optimizer_params']
+        train_opt = tf.keras.optimizers.SDG(
+            learning_rate=learning_rate, 
+            **opt_params)
+        
+        return loss_retval, learning_rate, train_opt 
+            
+    def build_train_targets(self, outputs, loss_retval, learning_rate, train_opt):
+        extra_targets_params = self.params['train_params']['targets']
+        train_targets = self._rep_loss_func(
+            outputs, 
+            **extra_targets_params)
+
+        train_targets['train_op'] = train_opt
+        train_targets['loss'] = loss_retval
+        train_targets['learning_rate'] = learning_rate
+
+        return train_targets
+
+    def train_func(self, inputs):
         outputs = self.build_network(inputs, train=True)
         loss_retval, learning_rate, train_opt = self.build_train_opt(outputs)     
         train_targets = self.build_train_targets(outputs, loss_retval, learning_rate, train_opt)
-        
         return train_targets
 
-    def run_train_loop(self):
+    def _run_train_loop(self):
         start_step = self.global_step
         train_loop = self.params['train_params'].get('train_loop')
         train_func = self.train_func
@@ -197,84 +211,58 @@ class UnPIE(tf.Module):
                 self.log_writer = open(self.log_file_path, 'a+')
 
             if curr_step % self.params['save_params']['save_valid_freq'] == 0:
-                for each_val_key in self.params['validation_params']:
-                    val_result = self.run_each_validation(each_val_key)
-                    self.val_log_writer.write(
-                            '%s: %s\n' % (each_val_key, str(val_result)))
-                    print(val_result)
+                val_result = self.run_each_validation()
+                self.val_log_writer.write(
+                        '%s: %s\n' % ('validation results: ', str(val_result)))
+                print(val_result)
                 self.val_log_writer.close()
                 self.val_log_writer = open(self.val_log_file_path, 'a+')
 
-    def run_test_loop(self):
-        for each_val_key in self.params['test_params']:
-            test_result = self.run_testing(each_val_key)
-            self.test_log_writer.write(
-                    '%s: %s\n' % (each_val_key, str(test_result)))
-            print(test_result)
+    def train(self):
+        print_separator('Starting UnPIE training')
+        self._run_train_loop()
+        print_separator('UnPIE training ended')
+
+
+    # Validation functions
+    def build_inference_targets(self, inputs, outputs):
+        target_params = self.params['inference_params']['targets']
+        targets = self._perf_func_kNN(inputs, outputs, **target_params)
+        return targets
+
+    def val_func(self, inputs):
+        outputs = self.build_network(inputs, train=False)
+        targets = self.build_inference_targets(inputs, outputs)
+        return targets
+
+    def run_each_validation(self):
+        agg_res = None
+        num_steps = self.params['validation_params']['num_steps']
+
+        for _step in tqdm.trange(num_steps):
+            res = self.params['validation_params']['valid_loop']['func'](
+                    self.val_func)
+            online_func = self.params['validation_params']['online_agg_func']
+            agg_res = online_func(agg_res, res, _step)
+
+        agg_func = self.params['validation_params']['agg_func']
+        val_result = agg_func(agg_res)
+        
+        return val_result
+
+
+    # Testing functions
+    def _run_test_loop(self):
+        test_result = self.run_testing()
+        self.test_log_writer.write(
+                '%s: %s\n' % ('test results: ', str(test_result)))
+        print(test_result)
         self.test_log_writer.close()
 
-    def build_val_inputs(self, val_key):
-        data_params = self.params['validation_params'][val_key]['data_params']
-        func = data_params.pop('func')
-        val_inputs = func(**data_params)
-        return val_inputs
-
-    def build_test_inputs(self, test_key):
-        data_params = self.params['test_params'][test_key]['data_params']
-        func = data_params.pop('func')
-        test_inputs = func(**data_params)
-        return test_inputs
-
-    def build_val_network(self, val_key, val_inputs):
-        with tf.name_scope('validation/' + val_key):
-            val_outputs = self.build_network(val_inputs, False)
-        return val_outputs
-    
-    def build_test_network(self, test_key, test_inputs):
-        with tf.name_scope('test/' + test_key):
-            test_outputs = self.build_network(test_inputs, False)
-        return test_outputs
-
-    def build_val_targets(self, val_key, val_inputs, val_outputs):
-        target_params = self.params['validation_params'][val_key]['targets']
-        func = target_params.pop('func')
-        val_targets = func(val_inputs, val_outputs, **target_params)
-        return val_targets
-    
-    def build_test_targets(self, test_key, test_inputs, test_outputs):
-        target_params = self.params['test_params'][test_key]['targets']
-        func = target_params.pop('func')
-        test_targets = func(test_inputs, test_outputs, **target_params)
-        return test_targets
-
-    def build_val(self):
-        tf.compat.v1.get_variable_scope().reuse_variables()
-        self.all_val_targets = {}
-        for each_val_key in self.params['validation_params']:
-            val_inputs = self.build_val_inputs(each_val_key)
-            val_outputs = self.build_val_network(each_val_key, val_inputs)
-            val_targets = self.build_val_targets(
-                    each_val_key, val_inputs, val_outputs)
-            self.all_val_targets[each_val_key] = val_targets
-
-    def build_test(self):
-        tf.compat.v1.get_variable_scope().reuse_variables()
-        self.all_test_targets = {}
-        for each_val_key in self.params['test_params']:
-            test_inputs = self.build_test_inputs(each_val_key)
-            test_outputs = self.build_test_network(each_val_key, test_inputs)
-            test_targets = self.build_test_targets(
-                    each_val_key, test_inputs, test_outputs)
-            self.all_test_targets[each_val_key] = test_targets           
-
-    def build_model(self):
-        self.build_train()
-        self.build_val()
-        if self.params['is_test']:
-            self.build_test()
-
-        self.build_sess_and_saver()
-        self.init_and_restore()
+    def test(self):
+        print_separator('Starting UnPIE testing')
+        self._run_test_loop()
+        print_separator('UnPIE testing ended')
 
 
     # Loss functions
@@ -322,14 +310,32 @@ class UnPIE(tf.Module):
             if key.startswith('loss_'):
                 ret_dict[key] = value
         return ret_dict
+    
 
-    # Running functions
-    def train(self):
-        print_separator('Starting UnPIE training')
-        self.run_train_loop()
-        print_separator('UnPIE training ended')
+    # Clustering functions
+    def _perf_func_kNN(
+            self,
+            inputs, output, 
+            instance_t,
+            k, val_num_clips,
+            num_classes):
+        curr_dist, all_labels = output
+        all_labels = tuple_get_one(all_labels)
+        top_dist, top_indices = tf.nn.top_k(curr_dist, k=k)
+        top_labels = tf.gather(all_labels, top_indices)
+        top_labels_one_hot = tf.one_hot(top_labels, num_classes)
+        top_prob = tf.exp(top_dist / instance_t)
+        top_labels_one_hot *= tf.expand_dims(top_prob, axis=-1)
+        top_labels_one_hot = tf.reduce_mean(top_labels_one_hot, axis=1)
+        top_labels_one_hot = tf.reshape(
+                top_labels_one_hot,
+                [-1, val_num_clips, num_classes])
+        top_labels_one_hot = tf.reduce_mean(top_labels_one_hot, axis=1)
+        _, curr_pred = tf.nn.top_k(top_labels_one_hot, k=1)
+        curr_pred = tf.squeeze(tf.cast(curr_pred, tf.int64), axis=1)
+        imagenet_top1 = tf.reduce_mean(
+                tf.cast(
+                    tf.equal(curr_pred, inputs['y']),
+                    tf.float32))
+        return {'top1_{k}NN'.format(k=k): imagenet_top1}
 
-    def test(self):
-        print_separator('Starting UnPIE testing')
-        self.run_test_loop()
-        print_separator('UnPIE testing ended')
