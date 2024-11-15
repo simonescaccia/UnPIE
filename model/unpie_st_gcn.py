dc48c1b790553ed6d3142893c1349dab6cb81388
Simone Scaccia <simonescaccia.s@gmail.com>
2024-11-15 13:12:29 +0100

    Add ST-GCN

---
+++ model/unpie_st_gcn.py
@@ -0,0 +1,165 @@
+import warnings
+import numpy as np
+import scipy as sp
+import tensorflow as tf
+
+from utils.graph_utils import normalized_adjacency
+
+REGULARIZER = tf.keras.regularizers.l2(l=0.0001)
+INITIALIZER = tf.keras.initializers.VarianceScaling(scale=2.,
+                                                    mode="fan_out",
+                                                    distribution="truncated_normal")
+
+"""The basic module for applying a spatial graph convolution.
+    Args:
+        filters (int): Number of channels produced by the convolution
+        kernel_size (int): Size of the graph convolving kernel
+    Shape:
+        - Input[0]: Input graph sequence in :math:`(N, C, T, V)` format
+        - Input[1]: Input graph adjacency matrix in :math:`(K, V, V)` format
+        - Output[0]: Output graph sequence in :math:`(N, out_channels, T, V)` format
+        - Output[1]: Graph adjacency matrix for output data in :math:`(K, V, V)` format
+        where
+            :math:`N` is a batch size
+            :math:`K` is the spatial kernel size
+            :math:`T` is a length of the sequence
+            :math:`V` is the number of graph nodes
+            :math:`C` is the number of incoming channels
+"""
+class SGCN(tf.keras.Model):
+    def __init__(self, filters, kernel_size=3):
+        super().__init__()
+        self.kernel_size = kernel_size
+        self.conv = tf.keras.layers.Conv2D(filters*kernel_size,
+                                           kernel_size=1,
+                                           padding='same',
+                                           kernel_initializer=INITIALIZER,
+                                           data_format='channels_first',
+                                           kernel_regularizer=REGULARIZER)
+
+    # N, C, T, V
+    def call(self, x, a):
+        x = self.conv(x)
+
+        N = tf.shape(x)[0]
+        C = tf.shape(x)[1]
+        T = tf.shape(x)[2]
+        V = tf.shape(x)[3]
+
+        x = tf.reshape(x, [N, self.kernel_size, C//self.kernel_size, T, V])
+        x = tf.einsum('nkctv,kvw->nctw', x, a)
+        return x, a
+
+
+"""Applies a spatial temporal graph convolution over an input graph sequence.
+    Args:
+        filters (int): Number of channels produced by the convolution
+        kernel_size (tuple): Size of the temporal convolving kernel and graph convolving kernel
+        stride (int, optional): Stride of the temporal convolution. Default: 1
+        activation (activation function/name, optional): activation function to use
+        residual (bool, optional): If ``True``, applies a residual mechanism. Default: ``True``
+        downsample (bool, optional): If ``True``, applies a downsampling residual mechanism. Default: ``True``
+                                     the value is used only when residual is ``True``
+    Shape:
+        - Input[0]: Input graph sequence in :math:`(N, in_channels, T_{in}, V)` format
+        - Input[1]: Input graph adjacency matrix in :math:`(K, V, V)` format
+        - Output[0]: Outpu graph sequence in :math:`(N, out_channels, T_{out}, V)` format
+        - Output[1]: Graph adjacency matrix for output data in :math:`(K, V, V)` format
+        where
+            :math:`N` is a batch size,
+            :math:`K` is the spatial kernel size, as :math:`K == kernel_size[1]`,
+            :math:`T_{in}/T_{out}` is a length of input/output sequence,
+            :math:`V` is the number of graph nodes.
+"""
+class STGCN(tf.keras.Model):
+    def __init__(self, filters, kernel_size=[9, 3], stride=1, activation='relu',
+                 residual=True, downsample=False):
+        super().__init__()
+        self.sgcn = SGCN(filters, kernel_size=kernel_size[1])
+
+        self.tgcn = tf.keras.Sequential()
+        self.tgcn.add(tf.keras.layers.BatchNormalization(axis=1))
+        self.tgcn.add(tf.keras.layers.Activation(activation))
+        self.tgcn.add(tf.keras.layers.Conv2D(filters,
+                                                kernel_size=[kernel_size[0], 1],
+                                                strides=[stride, 1],
+                                                padding='same',
+                                                kernel_initializer=INITIALIZER,
+                                                data_format='channels_first',
+                                                kernel_regularizer=REGULARIZER))
+        self.tgcn.add(tf.keras.layers.BatchNormalization(axis=1))
+
+        self.act = tf.keras.layers.Activation(activation)
+
+        if not residual:
+            self.residual = lambda x: 0
+        elif residual and stride == 1 and not downsample:
+            self.residual = lambda x: x
+        else:
+            self.residual = tf.keras.Sequential()
+            self.residual.add(tf.keras.layers.Conv2D(filters,
+                                                        kernel_size=[1, 1],
+                                                        strides=[stride, 1],
+                                                        padding='same',
+                                                        kernel_initializer=INITIALIZER,
+                                                        data_format='channels_first',
+                                                        kernel_regularizer=REGULARIZER))
+            self.residual.add(tf.keras.layers.BatchNormalization(axis=1))
+
+    def call(self, x, a):
+        res = self.residual(x)
+        x, a = self.sgcn(x, a)
+        x = self.tgcn(x)
+        x += res
+        x = self.act(x)
+        return x, a
+
+
+"""Spatial temporal graph convolutional networks.
+    Args:
+        num_class (int): Number of classes for the classification task
+    Shape:
+        - Input: :math:`(N, in_channels, T_{in}, V_{in})`
+        - Output: :math:`(N, num_class)` where
+            :math:`N` is a batch size,
+            :math:`T_{in}` is a length of input sequence,
+            :math:`V_{in}` is the number of graph nodes.
+"""
+class UnPIESTGCN(tf.keras.Model):
+    transform = normalized_adjacency
+
+    def __init__(self, input_dim, middle_dim, emb_dim):
+        super().__init__()
+
+        self.data_bn = tf.keras.layers.BatchNormalization(axis=1)
+
+        self.STGCN_layers = []
+        self.STGCN_layers.append(STGCN(input_dim, residual=False))
+        self.STGCN_layers.append(STGCN(input_dim))
+        self.STGCN_layers.append(STGCN(middle_dim, stride=2, downsample=True))
+        self.STGCN_layers.append(STGCN(middle_dim))
+        self.STGCN_layers.append(STGCN(emb_dim, stride=2, downsample=True))
+        self.STGCN_layers.append(STGCN(emb_dim))
+        self.STGCN_layers.append(STGCN(emb_dim))
+
+    def call(self, x, a):
+        N = tf.shape(x)[0]
+        C = tf.shape(x)[1]
+        T = tf.shape(x)[2]
+        V = tf.shape(x)[3]
+
+        x = tf.transpose(x, perm=[0, 3, 1, 2])
+        x = tf.reshape(x, [N, V * C, T])
+        x = self.data_bn(x)
+        x = tf.reshape(x, [N, V, C, T])
+        x = tf.transpose(x, perm=[0, 1, 3, 2])
+        x = tf.reshape(x, [N, C, T, V])
+
+        for layer in self.STGCN_layers:
+            x, a = layer(x, a)
+
+        # x: N,C,T,V
+        x = x[:, :, :, 0] # N,C,T, get the first node V (pedestrian) for each graph
+        x = tf.reshape(x, [N, T, C])
+
+        return x
--- utils/graph_utils.py
+++ utils/graph_utils.py
@@ -1,48 +1,40 @@
+import warnings
+
 import numpy as np
-import tensorflow as tf
-from spektral.data import Graph
-from spektral.data.utils import to_disjoint
-
-import numpy
-import sys
-numpy.set_printoptions(threshold=sys.maxsize)
-np.set_printoptions(linewidth=np.inf)
+import scipy as sp
 
 
-def graph_seq_to_graph(graph_seq, num_nodes):
-    '''
-    Convert a sequence of graphs to a DisjointGraph
-    '''        
-    x_list = []
-    a_list = []
-    y_list = []
-    for graph in graph_seq:
-        x, a, y = graph.x, graph.a, graph.y
-        x_list.append(x)
-        a_list.append(a)
-        y_list.append(y)
-    x, a, _ = to_disjoint(x_list, a_list)
+def degree_power(A, k):
+    r"""
+    Computes \(\D^{k}\) from the given adjacency matrix. Useful for computing
+    normalised Laplacian.
+    :param A: rank 2 array or sparse matrix.
+    :param k: exponent to which elevate the degree matrix.
+    :return: if A is a dense array, a dense array; if A is sparse, a sparse
+    matrix in DIA format.
+    """
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        degrees = np.power(np.array(A.sum(1)), k).ravel()
+    degrees[np.isinf(degrees)] = 0.0
+    if sp.issparse(A):
+        D = sp.diags(degrees)
+    else:
+        D = np.diag(degrees)
+    return D
 
-    graph = Graph(x=x, a=a, y=y_list[0])
-    return graph
 
-def graph_to_graph_seq(x, a):
-    '''
-    Convert a DisjointGraph to a sequence of graphs
-    - Graphs are undirected, so the adjacency matrix is symmetric
-    '''
-    graph_seq_batch = []
-    i = 1 # last node of the current graph
-    j = 0 # first node of the current graph
-    for i in range(a.shape[1]):
-        graph_seq = []
-        while i < a.shape[1]:
-            row = a[i, :i]
-            col = a[:i, i]
-            if np.sum(row) == 0 and np.sum(col) == 0:
-                graph_seq.append((x[j:i], a[j:i, j:i]))
-                j = i
-            i += 1
-        graph_seq_batch.append(graph_seq)
-    graph_seq_batch = np.array(graph_seq_batch)
-    return graph_seq_batch
+def normalized_adjacency(A, symmetric=True):
+    r"""
+    Normalizes the given adjacency matrix using the degree matrix as either
+    \(\D^{-1}\A\) or \(\D^{-1/2}\A\D^{-1/2}\) (symmetric normalization).
+    :param A: rank 2 array or sparse matrix;
+    :param symmetric: boolean, compute symmetric normalization;
+    :return: the normalized adjacency matrix.
+    """
+    if symmetric:
+        normalized_D = degree_power(A, -0.5)
+        return normalized_D.dot(A).dot(normalized_D)
+    else:
+        normalized_D = degree_power(A, -1.0)
+        return normalized_D.dot(A)
