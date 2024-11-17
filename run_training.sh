a7532094c0629eeef2828732aaaa07cbf48212cd
Simone Scaccia <simonescaccia.s@gmail.com>
2024-11-17 17:40:09 +0100

    Fix bugs

--- run_training.sh
+++ run_training.sh
@@ -2,8 +2,8 @@
 # python train_test.py 0 ${training_step_1}
 
 training_step_2=LA
-python3.10 train_test.py 1 ${training_step_2}
+python train_test.py 1 ${training_step_2}
 
 # python plot_creator.py ${training_step_1} ${training_step_2}
-python3.10 plot_creator.py ${training_step_2}
+python plot_creator.py ${training_step_2}
 
