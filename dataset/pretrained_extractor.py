74081dee393797478c06dde43fbede2c31230120
Simone Scaccia <simonescaccia.s@gmail.com>
2024-11-20 16:46:40 +0100

    Update feature extracto

--- dataset/pretrained_extractor.py
+++ dataset/pretrained_extractor.py
@@ -3,12 +3,20 @@ import tensorflow as tf
 
 class PretrainedExtractor(object):
     def __init__(self):
-
-        self.model = tf.keras.applications.VGG16(
-            weights='imagenet', 
+        
+        # self.model = tf.keras.applications.VGG16(
+        #     weights='imagenet', 
+        #     pooling='avg',
+        #     include_top=False
+        # )
+        self.model = tf.keras.applications.EfficientNetB5(
             include_top=False,
+            weights='imagenet',
             pooling='avg'
         )
+
+        #self.preprocess_func = tf.keras.applications.vgg16.preprocess_input
+        self.preprocess_func = tf.keras.applications.efficientnet.preprocess_input
 
     def __call__(self, inputs):
         x = self.model.predict(inputs, verbose=0)
@@ -16,6 +24,6 @@ class PretrainedExtractor(object):
     
     def preprocess(self, inputs):
         image_array = tf.keras.utils.img_to_array(inputs)
-        preprocessed_img = tf.keras.applications.vgg16.preprocess_input(image_array)
+        preprocessed_img = self.preprocess_func(image_array)
         expanded_img = np.expand_dims(preprocessed_img, axis=0)
         return expanded_img
