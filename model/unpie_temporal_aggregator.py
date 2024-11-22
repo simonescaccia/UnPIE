64845bdf1f346ba432ee542379d3d8cd21400f36
Simone Scaccia <simonescaccia.s@gmail.com>
2024-11-22 14:14:59 +0100

    Fix lstm

--- model/unpie_temporal_aggregator.py
+++ model/unpie_temporal_aggregator.py
@@ -1,10 +1,14 @@
 import tensorflow as tf
 
 class UnPIETemporalAggregator(tf.keras.Model):
-    def __init__(self, emb_dim, **kwargs):
+    def __init__(self,
+                 seq_len,
+                 emb_dim, **kwargs):
         super(UnPIETemporalAggregator, self).__init__(**kwargs)
-        
-        self.lstm = tf.keras.layers.LSTM(emb_dim)
+        self.lstm = tf.keras.layers.LSTM(
+            emb_dim,
+            input_shape=(seq_len, emb_dim)
+        )
     
     def __call__(self, x): # inputs shape: (batch_size, num_frames, emb_dim)
         x = self.lstm(x) # shape: (batch_size, emb_dim)
