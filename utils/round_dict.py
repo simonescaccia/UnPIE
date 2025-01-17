5a7c3cdff416cd57799398c0c22710e0a647d047
Simone Scaccia <simonescaccia.s@gmail.com>
2025-01-17 05:38:13 +0100

    Add experiments

--- utils/round_dict.py
+++ utils/round_dict.py
@@ -2,6 +2,7 @@ import numpy as np
 ########### vgg16 ############
 vgg_base_sup = {'Accuracy s.l.': 0.8283079060798548, 'F1 s.l.': 0.8908207334760737, 'ROC-AUC u.f.l.': 0.7810372841061803, 'Average Precision s.l.': 0.9152713409177777, 'Precision s.l.': 0.9312832316186938}
 vgg_abl_sup = {'Accuracy s.l.': 0.8249404491833031, 'F1 s.l.': 0.887861380559399, 'ROC-AUC u.f.l.': 0.7871441570241186, 'Average Precision s.l.': 0.9176754700224815, 'Precision s.l.': 0.9352615375365921}
+
 vgg_abl_ir_13_0_100 = {'Accuracy u.f.l.': 0.8184749319419238, 'F1 u.f.l.': 0.8836520032933399, 'ROC-AUC u.f.l.': 0.7783547978828325, 'Average Precision u.f.l.': 0.9144426189805774, 'Precision u.f.l.': 0.9320185102279288}
 vgg_abl_ir_12_0_100 = {'Accuracy u.f.l.': 0.8069731170598912, 'F1 u.f.l.': 0.8743492337441519, 'ROC-AUC u.f.l.': 0.7842599170093796, 'Average Precision u.f.l.': 0.9167461994933196, 'Precision u.f.l.': 0.9376921313757927}
 vgg_abl_ir_11_0_100 = {'Accuracy u.f.l.': 0.824805750907441, 'F1 u.f.l.': 0.8880868658758246, 'ROC-AUC u.f.l.': 0.7828244978296235, 'Average Precision u.f.l.': 0.915947015944829, 'Precision u.f.l.': 0.9329143482677816}
@@ -14,6 +15,17 @@ vgg_abl_ir_05_0_100 = {'Accuracy u.f.l.': 0.8208059210526316, 'F1 u.f.l.': 0.885
 vgg_abl_ir_04_0_100 = {'Accuracy u.f.l.': 0.8016674228675136, 'F1 u.f.l.': 0.868375401282314, 'ROC-AUC u.f.l.': 0.8077406018333749, 'Average Precision u.f.l.': 0.9261048839329634, 'Precision u.f.l.': 0.9525829647753293}
 vgg_abl_ir_03_0_100 = {'Accuracy u.f.l.': 0.8052220394736843, 'F1 u.f.l.': 0.8731443276875087, 'ROC-AUC u.f.l.': 0.7828889121389415, 'Average Precision u.f.l.': 0.9163135997949909, 'Precision u.f.l.': 0.9374179020772785}
 vgg_abl_ir_02_0_100 = {'Accuracy u.f.l.': 0.8178836774047188, 'F1 u.f.l.': 0.8825141259333621, 'ROC-AUC u.f.l.': 0.7875814833728094, 'Average Precision u.f.l.': 0.9180613606728178, 'Precision u.f.l.': 0.9373312316857525}
+
+vgg_abl_ir_04_9229_100 = {'Accuracy u.f.l.': 0.8145686819419238, 'F1 u.f.l.': 0.8802766108984901, 'ROC-AUC u.f.l.': 0.7825404208888642, 'Average Precision u.f.l.': 0.9158939841649802, 'Precision u.f.l.': 0.9348654157978049}
+vgg_abl_ir_04_4096_100 = {'Accuracy u.f.l.': 0.8260180353901997, 'F1 u.f.l.': 0.8889093768488946, 'ROC-AUC u.f.l.': 0.784107829386639, 'Average Precision u.f.l.': 0.9164187413844654, 'Precision u.f.l.': 0.9332893703600236}
+vgg_abl_ir_04_2048_100 = {'Accuracy u.f.l.': 0.8092629877495463, 'F1 u.f.l.': 0.8755067728206205, 'ROC-AUC u.f.l.': 0.7925669981204352, 'Average Precision u.f.l.': 0.9200078051974101, 'Precision u.f.l.': 0.941908851693255}
+vgg_abl_ir_04_1024_100 = {'Accuracy u.f.l.': 0.8159979015426498, 'F1 u.f.l.': 0.8813615440703619, 'ROC-AUC u.f.l.': 0.7845897000480282, 'Average Precision u.f.l.': 0.9167618880119226, 'Precision u.f.l.': 0.935801854766117}
+vgg_abl_ir_04_512_100 = {'Accuracy u.f.l.': 0.8244016560798548, 'F1 u.f.l.': 0.8878895536807023, 'ROC-AUC u.f.l.': 0.7813824185205107, 'Average Precision u.f.l.': 0.9154051140716, 'Precision u.f.l.': 0.9322297118869538}
+vgg_abl_ir_04_256_100 = {'Accuracy u.f.l.': 0.8079684664246825, 'F1 u.f.l.': 0.8739542753409928, 'ROC-AUC u.f.l.': 0.8008067172025928, 'Average Precision u.f.l.': 0.923149626792096, 'Precision u.f.l.': 0.9465698317036302}
+vgg_abl_ir_04_128_100 = {'Accuracy u.f.l.': 0.8003729015426498, 'F1 u.f.l.': 0.8676261166249911, 'ROC-AUC u.f.l.': 0.8050900172726863, 'Average Precision u.f.l.': 0.9253278966871115, 'Precision u.f.l.': 0.9516182160892606}
+vgg_abl_ir_04_64_100 = {'Accuracy u.f.l.': 0.8254792422867514, 'F1 u.f.l.': 0.8885850533761059, 'ROC-AUC u.f.l.': 0.7829478126647856, 'Average Precision u.f.l.': 0.915972103376972, 'Precision u.f.l.': 0.9328043625656387}
+vgg_abl_ir_04_1_100 = {'Accuracy u.f.l.': 0.8087766560798548, 'F1 u.f.l.': 0.8747273734093771, 'ROC-AUC u.f.l.': 0.7981911488915648, 'Average Precision u.f.l.': 0.9222554437989998, 'Precision u.f.l.': 0.9451308309419268}
+vgg_abl_ir_04_0_100 = {'Accuracy u.f.l.': 0.8016674228675136, 'F1 u.f.l.': 0.868375401282314, 'ROC-AUC u.f.l.': 0.8077406018333749, 'Average Precision u.f.l.': 0.9261048839329634, 'Precision u.f.l.': 0.9525829647753293}
 ########### effb3 ############
 eff_abl_sup_not_scaled = {'Accuracy s.l.': 0.8222558864265929, 'F1 s.l.': 0.8860498055178415, 'ROC-AUC u.f.l.': 0.7811706640931675, 'Average Precision s.l.': 0.9156947618073822, 'Precision s.l.': 0.9333000252796648}
 eff_abl_sup_scale = {'Accuracy s.l.': 0.7981749192059095, 'F1 s.l.': 0.8651596040016539, 'ROC-AUC u.f.l.': 0.8100376463853592, 'Average Precision s.l.': 0.9273146644871455, 'Precision s.l.': 0.9553160401795602}
@@ -26,6 +38,7 @@ def round_dict(dictionary):
 
 print("vgg_base: ", round_dict(vgg_base_sup.copy()))
 print("vgg_abl: ", round_dict(vgg_abl_sup.copy()))
+
 print("vgg_abl_ir_13_0_100: ", round_dict(vgg_abl_ir_13_0_100.copy()))
 print("vgg_abl_ir_12_0_100: ", round_dict(vgg_abl_ir_12_0_100.copy()))
 print("vgg_abl_ir_11_0_100: ", round_dict(vgg_abl_ir_11_0_100.copy()))
@@ -38,6 +51,17 @@ print("vgg_abl_ir_05_0_100: ", round_dict(vgg_abl_ir_05_0_100.copy()))
 print("vgg_abl_ir_04_0_100: ", round_dict(vgg_abl_ir_04_0_100.copy()))
 print("vgg_abl_ir_03_0_100: ", round_dict(vgg_abl_ir_03_0_100.copy()))
 print("vgg_abl_ir_02_0_100: ", round_dict(vgg_abl_ir_02_0_100.copy()))
+
+print("vgg_abl_ir_04_9229_100: ", round_dict(vgg_abl_ir_04_9229_100.copy()))
+print("vgg_abl_ir_04_4096_100: ", round_dict(vgg_abl_ir_04_4096_100.copy()))
+print("vgg_abl_ir_04_2048_100: ", round_dict(vgg_abl_ir_04_2048_100.copy()))
+print("vgg_abl_ir_04_1024_100: ", round_dict(vgg_abl_ir_04_1024_100.copy()))
+print("vgg_abl_ir_04_512_100: ", round_dict(vgg_abl_ir_04_512_100.copy()))
+print("vgg_abl_ir_04_256_100: ", round_dict(vgg_abl_ir_04_256_100.copy()))
+print("vgg_abl_ir_04_128_100: ", round_dict(vgg_abl_ir_04_128_100.copy()))
+print("vgg_abl_ir_04_64_100: ", round_dict(vgg_abl_ir_04_64_100.copy()))
+print("vgg_abl_ir_04_1_100: ", round_dict(vgg_abl_ir_04_1_100.copy()))
+print("vgg_abl_ir_04_0_100: ", round_dict(vgg_abl_ir_04_0_100.copy()))
 
 print("eff_abl_not_scaled", round_dict(eff_abl_sup_not_scaled.copy()))
 print("eff_abl_scale", round_dict(eff_abl_sup_scale.copy()))
