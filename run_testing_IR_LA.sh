7df319166b1ada24a733c99080bb600ae3ca569f
Simone Scaccia <simonescaccia.s@gmail.com>
2025-03-27 16:24:53 +0100

    Update cross validation

--- run_testing_IR_LA.sh
+++ run_testing_IR_LA.sh
@@ -1,6 +1,7 @@
 training_step_1=IR
 training_step_2=LA
-python train_test.py 2 ${training_step_1}
-python train_test.py 2 ${training_step_2}
+dataset=pie
+python train_test.py 2 ${training_step_1} ${dataset}
+python train_test.py 2 ${training_step_2} ${dataset}
 python plot_creator.py ${training_step_1} ${training_step_2}
 
